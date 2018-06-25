7219e788964adb2e4103962cdfe2b86ba0e4361d
Zach Simone <hi@zachsim.one>
2018-06-25 10:48:48 +1000

    Initial Commit

---
+++ Mute/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mute
+//
+//  Created by Zachary Simone on 25/6/18.
+//  Copyright © 2018 Zachary Simone. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mute/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mute
+//
+//  Created by Zachary Simone on 25/6/18.
+//  Copyright © 2018 Zachary Simone. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
