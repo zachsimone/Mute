b3710deaa00dd28a445b6b209a17148940779ff8
Zach Simone <hi@zachsim.one>
2020-02-16 08:30:04 +1100

    Rename ViewController to MainViewController.

--- Mute/MainViewController.h
+++ Mute/MainViewController.h
@@ -8,7 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface ViewController : NSViewController
+@interface MainViewController : NSViewController
 
 @property (weak) IBOutlet NSTextField *currentInputVolumeTextField;
 @property (weak) IBOutlet NSSlider *currentVolumeSlider;
--- Mute/MainViewController.m
+++ Mute/MainViewController.m
@@ -6,9 +6,9 @@
 //  Copyright © 2020 Zachary Simone. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
