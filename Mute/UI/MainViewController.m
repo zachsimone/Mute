aca6cefcf6e5863bb4792d88f4d362b7f67d20a3
Zach Simone <hi@zachsim.one>
2020-02-17 20:25:22 +1100

    Can now adjust input volume.

--- Mute/UI/MainViewController.m
+++ Mute/UI/MainViewController.m
@@ -7,20 +7,43 @@
 //
 
 #import "MainViewController.h"
+#import "AppleScriptController.h"
 
 @implementation MainViewController
 
+AppleScriptController *scriptController;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    // Do any additional setup after loading the view.
+    
+    scriptController = [[AppleScriptController alloc] init];
+    
+    self.currentVolumeSlider.minValue = 0;
+    self.currentVolumeSlider.maxValue = 100;
+    [self.currentVolumeSlider setContinuous:YES];
+    
+   //createInputDeviceArray();
+    
+    [self setValues];
 }
-
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
+}
+
+- (void)setValues {
+    UInt32 volume = [[scriptController getResponse:@"input volume of (get volume settings)"] int32Value];
+    self.currentVolumeSlider.doubleValue = volume;
+    NSString *labelText = [NSString stringWithFormat:@"Current input volume %i%%", volume];
+    [self.currentInputVolumeTextField setStringValue:labelText];
+}
+
+- (IBAction)sliderValueChanged:(NSSliderCell *)sender {
+    int inputVolumeValue = sender.intValue;
+    [scriptController setVolume:&inputVolumeValue];
+    [self setValues];
 }
 
 
